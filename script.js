cb7b02805a8c9363f3a48a2163924153be09e124
mdsaif123 <mdsaif62041@gmail.com>
2023-11-20 15:01:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+var istatus= document.getElementById("follow")
+var check=0
+
+istatus.addEventListener('click',function(){
+    if(check==0){
+        istatus.innerHTML="followed"
+        istatus.style.backgroundColor="yellow"
+        check=1
+    
+
+    }
+    else{
+        istatus.innerHTML="Follow+"
+        istatus.style.backgroundColor="red"
+        check=0
+    
+    }
+  
+})
+
+
+
+     
+        const darkMode = document.getElementById("btn");
+        const body = document.body;
+
+        darkMode.addEventListener("click", () => {
+            body.classList.toggle("dark-mode");
+        });
